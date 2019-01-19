a862eaf687be868ba94352defa2f8fc36bf53dee
aswinrprasad <aswinrprasad@gmail.com>
2019-01-19 16:21:02 +0530

    Data Science-LR Model

--- Ex1.R
+++ Ex1.R
@@ -17,6 +17,33 @@ t(t(names(boston.df))) #print the list in a useful column format(column names)
 plot(boston.df$MEDV~boston.df$LSTAT,xlab="MEDV",ylab="LSTAT") #Scatter plot with axes names
 hist(boston.df$MEDV,xlab="MEDV") #histogram of MEDV
 boxplot(boston.df$MEDV~boston.df$CHAS,xlab="CHAS",ylab="MEDV") #boxplot of MEDV for different values of CHAS
+set.seed(1) #to get the same parrtitions when re running the R code
+train.rows<-sample(rownames(boston.df),dim(boston.df)[1]*0.6) #partitioning into training 60% and validation 40%
+train.data<-boston.df[train.rows,] #collect all the columns with training row ID into training set
+valid.rows<-setdiff(rownames(boston.df),train.rows) #assign row ID that are not already in the training set, into validation
+valid.data <-boston.df[valid.rows,]
+valid.data<-boston.df[-train.rows,] #train.rows is string , so can't be used in this database
+
+#fit a linear model for Boston Dataset
+reg<-lm(MEDV~CRIM,data=boston.df,subset=train.rows) #Fit a linear model for MEDV and CRIM
+reg$coefficients #inspect the coefficients
+tr.res<-data.frame(train.data$MEDV,reg$fitted.values,reg$residuals)
+head(tr.res)
+
+pred<-predict(reg,newdata=valid.data)
+vl.res<-data.frame(valid.data$MEDV,pred,residuals=valid.data$MEDV - pred)
+head(vl.res)
+
+reg2<-lm(MEDV~CRIM+AGE,data=boston.df,subset=train.rows) #adding AGE too to the model increased the efficiency: more valid data added => more efficiency
+reg2$coefficients
+
+reg3<-lm(MEDV~CRIM+AGE+ZN+INDUS+NOX+CHAS+RM+DIS+RAD+TAX+PTRATIO+LSTAT,data=boston.df,subset=train.rows)
+reg3$coefficients
+#sample- command for taking a random sample from database
+
+args(lm)
+example(lm)
+?lm
 
 #https://r4ds.had.co.nz/index.html
 

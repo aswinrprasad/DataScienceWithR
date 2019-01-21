57a7fe8a0bffcd4fbfead9e690523f1ebddc4891
aswinrprasad <aswinrprasad@gmail.com>
2019-01-21 16:09:35 +0530

    R for data science

--- Ex1.R
+++ Ex1.R
@@ -1,4 +1,4 @@
-#class 1
+#CLASS 1
 
 boston.df <- read.csv("BostonHousing.csv",header = TRUE)
 dim(boston.df) #dimension of data frame
@@ -20,7 +20,7 @@ plot(boston.df$MEDV~boston.df$LSTAT,xlab="MEDV",ylab="LSTAT") #Scatter plot with
 hist(boston.df$MEDV,xlab="MEDV") #histogram of MEDV
 boxplot(boston.df$MEDV~boston.df$CHAS,xlab="CHAS",ylab="MEDV") #boxplot of MEDV for different values of CHAS
 
-#class 2
+#CLASS 2
 
 set.seed(1) #to get the same parrtitions when re running the R code
 train.rows<-sample(rownames(boston.df),dim(boston.df)[1]*0.6) #partitioning into training 60% and validation 40%
@@ -50,10 +50,39 @@ args(lm)
 example(lm)
 ?lm
 
-#class 3
+#CLASS 3
 
-library(psych)
-describe(boston.df)
+library(psych) #importing a library called psych
+describe(boston.df) #Summary of bostonhousing.csv dataset.
+mean(boston.df$CRIM)
+min(boston.df$CRIM)
+is.na(boston.df$CRIM) #Find the missing values , returns boolean
+sum(is.na(boston.df$CRIM)) #Find how many missing values are there.
+sapply(boston.df,mean) #Find mean of every column in the dataset (Can be done for min,max,median,standard dev)
+round(cor(boston.df),2) #Find the correlation matrix of the dataset
+
+boston.df2<-boston.df
+rows.to.missing<-sample(row.names(boston.df2),10) #Adding 10 missing values
+boston.df2[rows.to.missing,]$CRIM<-NA #adding NA
+summary(boston.df2$CRIM) 
+median(boston.df2$CRIM) #WIll not get output as there are NA values in the dataset now
+sum(is.na(boston.df2$CRIM)) #Result will be 10 , 10 NA values
+boston.df2[rows.to.missing,]$CRIM<-median(boston.df2$CRIM,na.rm=TRUE) #Substituting NA's with median or mean which is reffered to as IMPUTATION
+#na.rm=TRUE => Will not consider the missing values added for CRIM column because if considered the reult will be NA
+#Median is used as Mean values can cause errors if there are outliers in the data set.
+#Outliers : Data points in the graph that are way off 
+
+plot(boston.df$NOX~boston.df$LSTAT,ylab="NOX",xlab="LSTAT",col=ifelse(boston.df$CHAS,"black","gray"))
+data.for.plot<-aggregate(boston.df$MEDV,by = list(boston.df$CHAS),FUN = mean) #barchart of CHAS vs mean MEDV
+#compute mean MEDV
+names(data.for.plot)<-c("CHAS","MeanMEDV")
+barplot(data.for.plot$MeanMEDV,names.arg = data.for.plot$CHAS,xlab = "CHAS",ylab = "Avg.MEDV")
+#incomplete
+data.for.plot<-aggregate(boston.df$CAT.MEDV,by = list(boston.df$CHAS),FUN = mean)
+barplot(data.for.plot$MeanMEDV,names.arg = data.for.plot$CHAS,xlab = "CHAS",ylab = "perc.CAT.MEDV")
+#----
+
+
 
 #https://r4ds.had.co.nz/index.html
 
